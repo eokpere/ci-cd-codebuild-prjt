350fdb06b38bc3fd0277fa78735d121d12b5e061
eokpere <evelynokpere@outlook.com>
2022-09-28 21:01:51 -0400

    updates

--- cicd/apply-terraform.sh
+++ cicd/apply-terraform.sh
@@ -10,10 +10,7 @@ cd ..
 terraform init
 
 # # apply terraform
-terraform apply -auto-approve
-=======
 # terraform apply -auto-approve
->>>>>>> bda968b7219be9b8a62c36b091641d21c97e87af
 
 # destroy terraform
-# terraform destroy -auto-approve
+terraform destroy -auto-approve
